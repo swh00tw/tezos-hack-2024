d4d9e8c751f88c809f2122036b8c3390ff21af40
swh00tw <a6140000@gmail.com>
2024-03-02 14:39:40 -0500

    Add sponsor cards to rebate page

--- src/app/dashboard/rebates/page.tsx
+++ src/app/dashboard/rebates/page.tsx
@@ -1,3 +1,27 @@
+import { Grid, Button } from "@radix-ui/themes";
+import Image from "next/image";
+
+function SponsorCard() {
+  // random pick a number from 1 to 6
+  const randomIdx = Math.floor(Math.random() * 6) + 1;
+  const sponsorImage = `/rebate-${randomIdx}.png`;
+
+  return (
+    <div className="p-4 flex flex-col gap-y-4 justify-center items-center">
+      <Image src={sponsorImage} alt="sponsor image" width={150} height={150} />
+      <Button className="w-full">Buy</Button>
+    </div>
+  );
+}
+
 export default function Page() {
-  return <div> Rebates </div>;
+  return (
+    <div>
+      <Grid columns="3" gap="3" width="auto">
+        {Array.from({ length: 10 }).map((_, i) => (
+          <SponsorCard key={i} />
+        ))}
+      </Grid>
+    </div>
+  );
 }
