ee31dbd24b5f5b5abec8ccf9a9df7ff738ecb033
swh00tw <a6140000@gmail.com>
2024-03-01 21:15:54 -0500

    add theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <Theme accentColor="green">
+        <body className={inter.className}>{children}</body>
+      </Theme>
     </html>
   );
 }
