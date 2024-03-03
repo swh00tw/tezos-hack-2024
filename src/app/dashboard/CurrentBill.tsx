6134103f2d19f7adc03071c023cc3cb8b46257ca
Mr. Cain <58246556+DrDrewCain@users.noreply.github.com>
2024-03-02 20:52:26 -0500

    My mother can code better than me

--- src/app/dashboard/CurrentBill.tsx
+++ src/app/dashboard/CurrentBill.tsx
@@ -8,21 +8,7 @@ export default function CurrentBill() {
 
   return (
     <div>
-      <Button
-        onClick={() => {
-          Tezos.contract
-            .at("KT1M1XHYKuE9M4QZKxLGyXHPsh17cdbfNSA5")
-            .then((c) => {
-              console.log(c);
-              let methods = c.parameterSchema.ExtractSignatures();
-              console.log(JSON.stringify(methods, null, 2));
-              console.log(methods);
-            })
-            .catch((error) => console.log(`Error: ${error}`));
-        }}
-      >
-        Test
-      </Button>
+      nah
     </div>
   );
 }
